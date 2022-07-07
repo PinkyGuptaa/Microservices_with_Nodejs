738c31eb76466c27e6c42b883d63cb79647fe911
PinkyGuptaa <pinkyguptaa01@gmail.com>
2022-07-07 11:25:53 +0530

    first commit

---
+++ client/src/PostCreate.js
@@ -0,0 +1,53 @@
+import React, { useRef, useState, useEffect } from "react";
+import axios from "axios";
+import PostList from "./PostList";
+
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState({});
+  //const [add,setadd]= useState("false")
+  const add = useRef("false");
+
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+
+    setPosts(res.data);
+    console.log(posts);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+
+    setTitle("");
+    add.current = "true";
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label>Title</label>
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="form-control"
+            />
+          </div>
+          <button className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+      <PostList add={add.current} />
+    </>
+  );
+};
+
+export default PostCreate;
